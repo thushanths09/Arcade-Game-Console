a018f53573f1601c01c9cb2ae7cf9839614ebd05
Thushanth Sivashanthan <thushanths09@gmail.com>
2023-04-10 18:43:38 +0530

    variable name changes

--- main.py
+++ main.py
@@ -5,22 +5,30 @@ from tkinter import *
 # setting up the frame for dashboard
 root = Tk()
 root.title("Arcade Game Console")
-#bg = PhotoImage(file = "download.jpg")
+# bg = PhotoImage(file = "download.jpg")
 root.geometry('500x500')
-#frame1 = Frame(root, padding=20)
-#frame1.grid()
-#root.iconphoto(False, PhotoImage(file="E:\Studies\UOP\E20\Semester 2\GP 106 Computing\Project\Arcade-Game-Console\a.png"))
-# ttk.Label(root, text="Hello World!").grid(column=0,row=0)
-# ttk.Button(root, text="Quit", command=root.destroy).grid(column=1, row=0)
+# frame1 = Frame(root, padding=20)
+# frame1.place()
+# root.iconphoto(False, PhotoImage(file="E:\Studies\UOP\E20\Semester 2\GP 106 Computing\Project\Arcade-Game-Console\a.png"))
+# ttk.Label(root, text="Hello World!").place(x=0,y=0)
+# ttk.Button(root, text="Quit", command=root.destroy).place(x=1, y=0)
+
+# command function to run the game
+
 
 def run_A():
     subprocess.run(["python", "A_tictactoe.py"])
+
+
 def run_B():
     subprocess.run(["python", "B_hangmantile.py"])
+
+
 def run_C():
     subprocess.run(["python", "C_pianotiles.py"])
 
-#butttons for run the game
+
+# butttons for run the game
 bt1 = Button(
     root,
     text="TIC TAC TOE",
@@ -36,14 +44,16 @@ bt3 = Button(
     text="PIANO TILES",
     command=run_C
 )
-quit = Button(
+bt4 = Button(
     root,
     text="QUIT",
     command=root.destroy
 )
-bt1.grid(column=1, row=0)
-bt2.grid(column=4, row=0)
-bt3.grid(column=7, row=0)
-quit.grid(column=9, row=4)
+
+# placing the buttons in the frame
+bt1.place(x = 1, y = 0)
+bt2.place(x = 4, y = 0)
+bt3.place(x = 7, y = 0)
+bt4.place(x = 9, y = 4)
 
 root.mainloop()
